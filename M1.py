7885bec5e87ea21b9337618ec648118fa3ae6b15
Panda Bear <51027303+wx2123@users.noreply.github.com>
2021-12-26 16:49:06 -0500

    Update M1.py

--- M1.py
+++ M1.py
@@ -8,3 +8,37 @@ Created on Sun Dec 26 15:35:28 2021
 # 1.1.1.5 Importing a module | math
 import math
 print(math.sin(math.pi/2))
+
+# 1.1.1.6 Importing a module | math
+import math
+
+def sin(x):
+    if 2 * x == pi:
+        return 0.99999999
+    else:
+        return None
+
+pi = 3.14
+
+print(sin(pi/2))
+print(math.sin(math.pi/2))
+
+# 1.1.1.7 Importing a module | math
+from math import sin, pi
+print(sin(pi/2))
+
+
+# 1.1.1.8 Importing a module | math
+pi = 3.14
+
+def sin(x):
+    if 2 * x == pi:
+        return 0.99999999
+    else:
+        return None
+print(sin(pi / 2))
+
+from math import sin, pi
+
+print(sin(pi / 2))
+
