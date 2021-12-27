bf0523b8f658ea98ae1b25b1379d3c0d3b94b79d
Panda Bear <51027303+wx2123@users.noreply.github.com>
2021-12-27 11:19:08 -0500

    Update M1.py

--- M1.py
+++ M1.py
@@ -42,3 +42,53 @@ from math import sin, pi
 
 print(sin(pi / 2))
 
+
+import math
+for name in dir(math):
+    print(name, end="\t")
+
+import math
+dir(math)
+
+
+# 1.2.1.4 Useful modules | math
+from math import ceil, floor, trunc
+
+x = 1.4
+y = 2.6
+
+print(floor(x), floor(y))
+print(floor(-x), floor(-y))
+print(ceil(x), ceil(y))
+print(ceil(-x), ceil(-y))
+print(trunc(x), trunc(y))
+print(trunc(-x), trunc(-y))
+
+# 1.2.1.6 Useful modules | random
+
+from random import random, seed
+
+seed(0)
+
+for i in range(5):
+    print(random())
+
+# 1.2.1.7 Useful modules | random
+from random import randrange, randint
+
+print(randrange(1), end=' ')
+print(randrange(0, 1), end=' ')
+print(randrange(0, 1, 1), end=' ')
+print(randint(0, 1))
+
+
+# 1.2.1.8 Useful modules | random
+
+from random import choice, sample
+
+my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+print(choice(my_list))
+print(sample(my_list, 5))
+print(sample(my_list, 10))
+
