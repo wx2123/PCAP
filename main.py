8b549fa78576dec394810a2248c4a0bcc3187f9b
Panda Bear <51027303+wx2123@users.noreply.github.com>
2021-12-27 23:55:34 -0500

    12/27

---
+++ main.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 27 22:30:15 2021
+
+@author: xuewu
+"""
+
+import module
---
+++ module.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 27 22:27:51 2021
+
+@author: xuewu
+"""
+
+if __name__ == "__main__":
+    print("I prefer to be a module.")
+else:
+    print("I like to be a module.")
+
