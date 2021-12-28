b5a4995a60ec9be8e03597bdefe91914a6ad3e99
Panda Bear <51027303+wx2123@users.noreply.github.com>
2021-12-28 12:21:21 -0500

    12、28

--- main.py
+++ main.py
@@ -5,4 +5,10 @@ Created on Mon Dec 27 22:30:15 2021
 @author: xuewu
 """
 
-import module
+from module import suml, prodl
+
+zeroes = [0 for i in range(5)]
+ones = [1 for i in range(5)]
+print(suml(zeroes))
+print(prodl(ones))
+
