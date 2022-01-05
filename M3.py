3ac9e5f4aebccde9cffe857bc39b54953b106939
Panda Bear <51027303+wx2123@users.noreply.github.com>
2022-01-04 23:22:58 -0500

    Update M3.py

--- M3.py
+++ M3.py
@@ -86,3 +86,28 @@ stack_object_1.push(3)
 stack_object_2.push(stack_object_1.pop())
 
 print(stack_object_2.pop())
+
+
+# 3.2.1.9 A short journey from procedural to object approach
+class Stack:
+    def __init__(self):
+        self.__stack_list = []
+
+    def push(self, val):
+        self.__stack_list.append(val)
+
+    def pop(self):
+        val = self.__stack_list[-1]
+        del self.__stack_list[-1]
+        return val
+
+little_stack = Stack()
+another_stack = Stack()
+funny_stack = Stack()
+
+little_stack.push(1)
+another_stack.push(little_stack.pop() + 1)
+funny_stack.push(another_stack.pop() - 2)
+
+print(funny_stack.pop())
+
