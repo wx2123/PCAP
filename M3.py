0ba68dbbb609104949c4c90edeb91e5a5f6929a4
Panda Bear <51027303+wx2123@users.noreply.github.com>
2022-01-12 20:37:45 -0500

    Update M3.py

--- M3.py
+++ M3.py
@@ -435,5 +435,187 @@ print(obj.__dict__)
 incIntsI(obj)
 print(obj.__dict__)
 
+# 3.5.1.1 OOP Fundamentals: Inheritance
 
+class Star:
+    def __init__(self, name, galaxy):
+        self.name = name
+        self.galaxy = galaxy
+
+sun = Star("Sun", "Milky Way")
+print(sun)
+
+# 3.5.1.2 OOP Fundamentals: Inheritance
+class Star:
+    def __init__(self, name, galaxy):
+        self.name = name
+        self.galaxy = galaxy
+
+    def __str__(self):
+        return self.name + ' in ' + self.galaxy
+
+sun = Star("Sun", "Milky Way")
+print(sun)
+
+
+# 3.5.1.4 OOP Fundamentals: Inheritance
+class Vehicle:
+    pass
+
+
+class LandVehicle(Vehicle):
+    pass
+
+
+class TrackedVehicle(LandVehicle):
+    pass
+
+
+for cls1 in [Vehicle, LandVehicle, TrackedVehicle]:
+    for cls2 in [Vehicle, LandVehicle, TrackedVehicle]:
+        print(issubclass(cls1, cls2), end="\t")
+    print()
+
+# 3.5.1.5 OOP Fundamentals: Inheritance
+class Vehicle:
+    pass
+
+
+class LandVehicle(Vehicle):
+    pass
+
+
+class TrackedVehicle(LandVehicle):
+    pass
+
+
+my_vehicle = Vehicle()
+my_land_vehicle = LandVehicle()
+my_tracked_vehicle = TrackedVehicle()
+
+for obj in [my_vehicle, my_land_vehicle, my_tracked_vehicle]:
+    for cls in [Vehicle, LandVehicle, TrackedVehicle]:
+        print(isinstance(obj, cls), end="\t")
+    print()
+
+# # 3.5.1.6 OOP Fundamentals: Inheritance
+class SampleClass:
+    def __init__(self, val):
+        self.val = val
+
+object_1 = SampleClass(0)
+object_2 = SampleClass(2)
+object_3 = object_1
+object_3.val += 1
+
+print(object_1 is object_2)
+print(object_2 is object_3)
+print(object_3 is object_1)
+print(object_1.val, object_2.val, object_3.val)
+
+string_1 = "Mary had a little "
+string_2 = "Mary had a little lamb"
+string_1 += "lamb"
+
+print(string_1 == string_2, string_1 is string_2)
+
+
+# # 3.5.1.7 OOP Fundamentals: Inheritance
+class Super:
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        return "My name is " + self.name + "."
+
+
+class Sub(Super):
+    def __init__(self, name):
+        Super.__init__(self, name)
+
+
+obj = Sub("Andy")
+
+print(obj)
+
+# # 3.5.1.8 OOP Fundamentals: Inheritance
+class Super:
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        return "My name is " + self.name + "."
+
+class Sub(Super):
+    def __init__(self, name):
+        super().__init__(name)
+
+obj = Sub("Andy")
+
+print(obj)
+
+# # 3.5.1.9 OOP Fundamentals: Inheritance
+# Testing properties: class variables.
+class Super:
+    supVar = 1
+
+
+class Sub(Super):
+    subVar = 2
+
+
+obj = Sub()
+
+print(obj.subVar)
+print(obj.supVar)
+
+# # 3.5.1.10 OOP Fundamentals: Inheritance
+# Testing properties: instance variables.
+class Super:
+    def __init__(self):
+        self.supVar = 11
+
+class Sub(Super):
+    def __init__(self):
+        super().__init__()
+        self.subVar = 12
+
+obj = Sub()
+
+print(obj.subVar)
+print(obj.supVar)
+
+
+# # 3.5.1.11 OOP Fundamentals: Inheritance
+class Level1:
+    variable_1 = 100
+    def __init__(self):
+        self.var_1 = 101
+
+    def fun_1(self):
+        return 102
+
+class Level2(Level1):
+    variable_2 = 200
+    def __init__(self):
+        super().__init__()
+        self.var_2 = 201
+    
+    def fun_2(self):
+        return 202
+
+class Level3(Level2):
+    variable_3 = 300
+    def __init__(self):
+        super().__init__()
+        self.var_3 = 301
+
+    def fun_3(self):
+        return 302
+
+obj = Level3()
+
+print(obj.variable_1, obj.var_1, obj.fun_1())
+print(obj.variable_2, obj.var_2, obj.fun_2())
+print(obj.variable_3, obj.var_3, obj.fun_3())
 
