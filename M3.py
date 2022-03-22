0b8d1d85d89fd8f628495812a2d8f51ced395e04
Panda Bear <51027303+wx2123@users.noreply.github.com>
2022-03-21 21:11:56 -0400

    Update M3.py

--- M3.py
+++ M3.py
@@ -1005,13 +1005,11 @@ except Exception as e:
 class MyZeroDivisionError(ZeroDivisionError):	
     pass
 
-
 def do_the_division(mine):
     if mine:
         raise MyZeroDivisionError("some worse news")
     else:		
         raise ZeroDivisionError("some bad news")
-
 
 for mode in [False, True]:
     try:
